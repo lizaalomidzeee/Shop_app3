3b15e0145135dafe95f8f9a09d7e88c9a13fd1dc
lizaalomidzeee <lomidzelizi997@gmail.com>
2024-12-23 21:31:11 +0400

    create custom manager objects for each model

---
+++ shop/management/commands/custom_managers_task.py
@@ -0,0 +1,192 @@
+from django.core.management import BaseCommand
+from shop.models import Item, Tag, Category
+from django.db.models import Count, Min, Max, Avg, Sum
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        categories = Category.objects.with_item_count()
+        for category in categories:
+            print(f"Category: {category.name}, Item Count: {category.item_count}")
+            
+            
+        items = Item.objects.with_tag_count()
+        for item in items:
+            print(f"Item: {item.name}, Tags Count: {item.tags_count}")
+            
+            
+        tags = Tag.objects.popular_tags(min_items=2)
+        for tag in tags:
+            print(f"Tag: {tag.name}, Item Count: {tag.item_count}")
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # items = Item.objects.select_related('category')
+        # for item in items:
+        #     print(f"Item: {item.name}, Category: {item.category.name}")
+        
+        
+        
+        
+        # items = Item.objects.prefetch_related('tags')
+        # for item in items:
+        #     tags = Item.tags.all()
+        #     tag_names = [tag.name for tag in tags]
+        #     print(f"Item: {item.name}, Tag: {','.join(tag_names)}")
+
+            
+        
+        
+        
+        # price_info = Item.objects.aggregate(
+        #     max_price = Max('price'),
+        #     min_price = Min('price'),
+        #     avg_price = Avg('price')
+        # )
+        # print(f"Max price: {price_info['max_price']}")
+        # print(f"Min price: {price_info['min_price']}")
+        # print(f"Avg price: {price_info['avg_price']}")
+        
+        
+        
+        
+        # category = Category.objects.first()
+        # category_item_count = category.items.aggregate(total_items=Count('id'))
+        # print(f"Category '{category.name}' has {category_item_count['total_items']} items")
+
+        
+        
+
+        # categories = Category.objects.annotate(
+        #     items_count = Count('items'),
+        #     items_price_sum = Sum('items__price', default=0)
+        # ) 
+        
+        # for category in categories:
+        #     print(f'Category: {category.name}')
+        #     print(f'Total items: {category.items_count}')
+        #     print(f'Total price: {category.items_price_sum}')
+        
+        
+        
+
+        
+        
+        
--- shop/models.py
+++ shop/models.py
@@ -1,9 +1,38 @@
 from django.db import models
+from django.db.models import Count
+
+
+
+class CategoryManager(models.Manager):
+    def with_item_count(self):
+        return self.annotate(item_count=Count("item"))
+    
+    
+class ItemManager(models.Manager):
+    def with_tag_count(self):
+        return self.annotate(tag_count=Count("tags"))
+    
+    
+class TagManager(models.Manager):
+    def popular_tags(self, min_items):
+        return self.annotate(item_count=Count("item")).filter(item_count__gte=min_items)
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
+    objects = CategoryManager()
 
+    def __str__(self):
+        return self.name
+    
+    
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    items = models.ManyToManyField(Item, related_name='tags')
+    
+    objects = TagManager()
+    
     def __str__(self):
         return self.name
     
@@ -12,16 +41,13 @@ class Category(models.Model):
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='items')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='item')
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField(blank=True, null=True)
-    tags = models.ManyToManyField(Tag, related_name='items', blank=True)
+
+    
+    objects = ItemManager()
 
     def __str__(self):
         return self.name
     
-class Tag(models.Model):
-    name = models.CharField(max_length=50)
-    
-    def __str__(self):
-        return self.name
+
