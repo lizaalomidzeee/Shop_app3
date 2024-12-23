6b5058912807eca87b50182ade0cdf0aad7ffe53
lizaalomidzeee <lomidzelizi997@gmail.com>
2024-12-23 22:21:30 +0400

    ადმინპანელი

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from shop.models import Category, Item, Tag
 
-# Register your models here.
+class ItemInline(admin.TabularInline):
+    model = Item
+    extra = 1
+    
+class TagInline(admin.StackedInline): 
+    model = Item.tags.through
+    extra = 1
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+    ordering = ('name',)
+    inlines = [ItemInline]
+    
+admin.site.register(Category, CategoryAdmin)
+    
+    
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'description')
+    search_fields = ('name', 'description')
+    ordering = ('-price',)
+    inlines = [TagInline]
+    
+admin.site.register(Item, ItemAdmin)
+    
+    
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    
+admin.site.register(Tag, TagAdmin)
--- shop/models.py
+++ shop/models.py
@@ -1,53 +1,92 @@
 from django.db import models
-from django.db.models import Count
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 
-
-
-class CategoryManager(models.Manager):
-    def with_item_count(self):
-        return self.annotate(item_count=Count("item"))
-    
-    
-class ItemManager(models.Manager):
-    def with_tag_count(self):
-        return self.annotate(tag_count=Count("tags"))
-    
-    
-class TagManager(models.Manager):
-    def popular_tags(self, min_items):
-        return self.annotate(item_count=Count("item")).filter(item_count__gte=min_items)
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    objects = CategoryManager()
+    images = GenericRelation('Image', related_query_name="category")
 
     def __str__(self):
         return self.name
-    
-    
-    
-class Tag(models.Model):
-    name = models.CharField(max_length=50)
-    items = models.ManyToManyField(Item, related_name='tags')
-    
-    objects = TagManager()
-    
-    def __str__(self):
-        return self.name
-    
-    
-
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='item')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='items')
     price = models.DecimalField(max_digits=10, decimal_places=2)
-
-    
-    objects = ItemManager()
+    description = models.TextField(blank=True, null=True)
+    images = GenericRelation("Image", related_query_name="item")
 
     def __str__(self):
         return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    items = models.ManyToManyField(Item, related_name='tags', blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Image(models.Model):
+    url = models.URLField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+
+
+# from django.db import models
+# from django.db.models import Count
+# from shop.models import Category, Item, Tag
+
+
+
+# class CategoryManager(models.Manager):
+#     def with_item_count(self):
+#         return self.annotate(item_count=Count("item"))
+    
+    
+# class ItemManager(models.Manager):
+#     def with_tag_count(self):
+#         return self.annotate(tag_count=Count("tags"))
+    
+    
+# class TagManager(models.Manager):
+#     def popular_tags(self, min_items):
+#         return self.annotate(item_count=Count("item")).filter(item_count__gte=min_items)
+
+# class Category(models.Model):
+#     name = models.CharField(max_length=255)
+#     description = models.TextField(blank=True, null=True)
+#     objects = CategoryManager()
+
+#     def __str__(self):
+#         return self.name
+    
+    
+    
+# class Tag(models.Model):
+#     name = models.CharField(max_length=50)
+#     items = models.ManyToManyField(Item, related_name='tags')
+    
+#     objects = TagManager()
+    
+#     def __str__(self):
+#         return self.name
+    
+    
+
+
+# class Item(models.Model):
+#     name = models.CharField(max_length=255)
+#     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='item')
+#     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    
+#     objects = ItemManager()
+
+#     def __str__(self):
+#         return self.name
     
 
